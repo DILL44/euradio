1eea3997816ac8a2cd8b892eb9c53ff3ffaabf99
Antonin Jourdan <antonin.jourdan@webcraft-studio.com>
2012-08-16 13:24:51 +0200

    fixed news twigs

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,6 +11,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
+    	'88.140.116.45'
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
