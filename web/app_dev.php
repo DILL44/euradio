b55c22000287a183bc6a500bd89c37c845a25c78
Antonin Jourdan <antonin.jourdan@webcraft-studio.com>
2012-08-17 15:35:44 +0200

    EOW - clean and tidy ! ready for work.

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,4 +1,5 @@
 <?php
+umask(0002);
 
 // if you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
