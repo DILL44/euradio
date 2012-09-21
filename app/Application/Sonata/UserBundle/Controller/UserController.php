594d638c0618ba1a47a15aa4441b2b655b83233e
koalas <npatard@gmail.com>
2012-09-21 17:09:33 +0200

    add user list

--- app/Application/Sonata/UserBundle/Controller/UserController.php
+++ app/Application/Sonata/UserBundle/Controller/UserController.php
@@ -3,23 +3,18 @@
 namespace Application\Sonata\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 use Application\Sonata\UserBundle\Entity\User;
 
 /**
  * User controller.
  *
- * @Route("/user")
  */
 class UserController extends Controller
 {
     /**
      * Lists all User entities.
      *
-     * @Route("/", name="user")
-     * @Template()
      */
     public function indexAction()
     {
@@ -27,28 +22,8 @@ class UserController extends Controller
 
         $entities = $em->getRepository('ApplicationSonataUserBundle:User')->findAll();
 
-        return array('entities' => $entities);
+        return $this->render('ApplicationSonataUserBundle:User:index.html.twig', array(
+            'entities' => $entities
+        ));
     }
-
-    /**
-     * Finds and displays a User entity.
-     *
-     * @Route("/{id}/show", name="user_show")
-     * @Template()
-     */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $entity = $em->getRepository('ApplicationSonataUserBundle:User')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find User entity.');
-        }
-
-        return array(
-            'entity'      => $entity,
-        );
-    }
-
 }
